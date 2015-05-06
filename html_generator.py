ef39c4dd104d7a3c4d42f38d7fc89c5687677fbd
matth0604 <matth0604@hotmail.com>
2015-05-06 19:42:23 -0400

    Committed by Udacity

---
+++ html_generator.py
@@ -0,0 +1,102 @@
+def generate_concept_HTML(concept_title, concept_description):
+    html_text_1 = '''
+<div class="concept">
+    <div class="concept-title">
+        ''' + concept_title
+    html_text_2 = '''
+    </div>
+    <div class="concept-description">
+        ''' + concept_description
+    html_text_3 = '''
+    </div>
+</div>'''
+    
+    full_html_text = html_text_1 + html_text_2 + html_text_3
+    return full_html_text
+
+def get_title(concept):
+    start_location = concept.find('TITLE:')
+    end_location = concept.find('DESCRIPTION:')
+    title = concept[start_location+7 : end_location-1]
+    return title
+
+def get_description(concept):
+    start_location = concept.find('DESCRIPTION:')
+    description = concept[start_location+13 :]
+    return description
+
+def get_concept_by_number(text, concept_number):
+    counter = 0
+    while counter < concept_number:
+        counter = counter + 1
+        next_concept_start = text.find('TITLE:')
+        next_concept_end   = text.find('TITLE:', next_concept_start + 1)
+        concept = text[next_concept_start:next_concept_end]
+        text = text[next_concept_end:]
+    return concept
+        
+
+Python_Notes = """TITLE: Python Programming
+DESCRIPTION: The phrase "computers are stupid" refers 
+to how they interpret instructions literally. This 
+means that small typos can cause big problems.
+TITLE: Python
+DESCRIPTION: Python is a "programming language." It 
+provides programmers a way to write instructions for a 
+computer to execute in a way that the computer can understand.
+TITLE: While Loops
+DESCRIPTION: A while loop repeatedly executes the body of
+the loop until the "test condition" is no longer true."""
+
+
+def generate_all_html(text):
+    current_concept_number = 1
+    concept = get_concept_by_number(text, current_concept_number)
+    all_html = ''
+    while concept != '':
+        title = get_title(concept)
+        description = get_description(concept)
+        concept_html = generate_concept_HTML(title, description)
+        all_html = all_html + concept_html
+        current_concept_number = current_concept_number + 1
+        concept = get_concept_by_number(text, current_concept_number)
+    return all_html
+
+
+print generate_all_html(Python_Notes)
+
+def generate_concept_HTML(concept_title, concept_description):
+    html_text_1 = '''
+<div class="concept">
+    <div class="concept-title">
+        ''' + concept_title
+    html_text_2 = '''
+    </div>
+    <div class="concept-description">
+        ''' + concept_description
+    html_text_3 = '''
+    </div>
+</div>'''
+    full_html_text = html_text_1 + html_text_2 + html_text_3
+    return full_html_text
+
+def make_HTML(concept):
+    concept_title = concept[0]
+    concept_description = concept[1]
+    return generate_concept_HTML(concept_title, concept_description)
+
+EXAMPLE_LIST_OF_CONCEPTS = [ ['Python', 'Python is a Programming Language'],
+                             ['For Loop', 'For Loops allow you to iterate over lists'],
+                             ['Lists', 'Lists are sequences of data'] ]
+
+
+def make_HTML_for_many_concepts(list_of_concepts):
+    HTML = ""
+    for concept in list_of_concepts:
+        concept_HTML = make_HTML(concept)
+        HTML = HTML + concept_HTML
+    return HTML
+
+print make_HTML_for_many_concepts(EXAMPLE_LIST_OF_CONCEPTS)
+
+
